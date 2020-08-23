027697026b08718e75fe72970560ab652675c89e
jonoliver82 <jonoliver82@gmail.com>
2020-08-23 23:56:41 +0100

    Format means, add min and max per month/week, filter most watched to only those > 1

--- letterboxd.R
+++ letterboxd.R
@@ -1,4 +1,6 @@
 # Perform analytics on Letterboxd profile data obtained via Settings - Export
+rm(list = ls())
+
 library(dplyr)
 library(lubridate)
 
@@ -14,24 +16,34 @@ year$Watched.Date <- as.Date(year$Watched.Date)
 
 monthly <- year %>% dplyr::group_by(month=floor_date(Watched.Date, "month"))
 monthly_sum <- summarise(monthly, count=n())
+monthly_sum$month <- format(monthly_sum$month, "%b")
 barplot(monthly_sum$count, names=monthly_sum$month, main="Watches per Month")
 
 weekly <- year %>% dplyr::group_by(week=floor_date(Watched.Date, "week"))
 weekly_sum <- summarise(weekly, count=n())
-barplot(weekly_sum$count, names=weekly_sum$week, main="Watches per Week")
+barplot(weekly_sum$count, names=weekly_sum$week, main="Watches per Week", xaxt='n')
 
 # 3 Average Films Watched per month and per week
-print(paste0("Average per month: ", mean(summarise(monthly, count=n())$count)))
-print(paste0("Average per week: ", mean(summarise(weekly, count=n())$count)))
+print(paste0("Average per month: ", format(mean(monthly_sum$count), digits = 1, nsmall = 0)))
+monthmin <- which.min(monthly_sum$count)
+print(paste0("Min per month: ", monthly_sum$count[monthmin], " in ", monthly_sum$month[monthmin]))
+monthmax <- which.max(monthly_sum$count)
+print(paste0("Max per month: ", monthly_sum$count[monthmax], " in ", monthly_sum$month[monthmax]))
+
+print(paste0("Average per week: ", format(mean(weekly_sum$count), digits = 1, nsmall = 0)))
+weekmin <- which.min(weekly_sum$count)
+print(paste0("Min per week: ", weekly_sum$count[weekmin], " in ", weekly_sum$week[weekmin]))
+weekmax <- which.max(weekly_sum$count)
+print(paste0("Max per week: ", weekly_sum$count[weekmax], " in ", weekly_sum$week[weekmax]))
 
 # 4 Films watched per day of week
-year$dow <- wday(year$Watched.Date, label = TRUE, abbr = TRUE)
+year$dow <- wday(year$Watched.Date, label = TRUE, abbr = TRUE, week_start = 1)
 daily <- year %>% dplyr::group_by(dow)
 daily_sum <- summarise(daily, count=n())
 barplot(daily_sum$count, names=daily_sum$dow, main="Watches per Day of Week")
 
 # 4 Pie Chart of Watches v Re-watches
-pie(table(year$Rewatch),c("Watches","Re-watches"), main="Watches and Re-watches")
+pie(table(year$Rewatch),labels = c("Watches","Re-watches"), main="Watches and Re-watches")
 
 # 5 Column Chart of ratings spread
 ratings <- year %>% dplyr::group_by(year$Rating)
@@ -39,9 +51,13 @@ ratings_sum <- summarise(ratings, count=n())
 barplot(ratings_sum$count, names=ratings_sum$`year$Rating`, main="Ratings Spread")
 
 # 6 List of most watched films this year (names, counts)
-print(paste0("Most watched filmes in ", current_year))
-print(head(sort(table(year$Name), decreasing = TRUE), 5))
+print(paste0("Most watched films in ", current_year))
+yeardf <- as.data.frame(table(year$Name)) %>% rename(Name = Var1)
+yeardf <- filter(yeardf, yeardf$Freq > 1)
+print(head(yeardf[order(yeardf$Freq, decreasing = TRUE),], 5))
 
 # 7 List of most watched films of all time (names, counts)
 print("Most watched films")
-print(head(sort(table(diary$Name), decreasing = TRUE), 20))
+diarydf <- as.data.frame(table(diary$Name)) %>% rename(Name = Var1)
+diarydf <- filter(diarydf, diarydf$Freq > 1)
+print(head(diarydf[order(diarydf$Freq, decreasing = TRUE),], 20))
